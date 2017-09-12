fd646366310f6b6a2618451ae5d0dc0acbdfb2fb
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-09-12 06:54:25 -0300

    Crawler is now following links inside same domain

--- src/crawler/crawler/spiders/questions.py
+++ src/crawler/crawler/spiders/questions.py
@@ -3,12 +3,21 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "questions"
+
     start_urls = [
         'http://codeforces.com/',
     ]
 
+    allowed_domains = [
+        'codeforces.com',
+    ]
+
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = 'documents/%s.html' % page
+        page = response.url.split('/')
+        page.pop(0)
+        name = '-'.join(page)
+        filename = 'documents/%s.html' % name
         with open(filename, 'wb') as f:
             f.write(response.body)
+        for href in response.css('a::attr(href)'):
+            yield response.follow(href, callback=self.parse)
