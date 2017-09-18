54ae7dbee50ca1062ace6f64471f9b2c4a2d7207
Arthurlpgc <arthurlpgcosta@gmail.com>
2017-09-17 21:06:38 -0300

    feature: add spoj

--- src/extractor/extractor.py
+++ src/extractor/extractor.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import json
 
 class CodeforcesExtractor:
 	def process(self, html):
@@ -35,26 +36,63 @@ class CodechefExtractor:
 		problem['statement'] = statement
 		return problem
 
+class SpojExtractor:
+	def get_td(self, html, label):
+		trs=html.find_all('tr')
+		for tr in trs:
+			if len(tr.find_all('td'))==2 and tr.find_all('td')[0].text==label:
+				return tr.find_all('td')[1].text.strip()
+	def process(self, html):
+		problem={}
+		problem['title']=html.find(id="problem-name").string
+		problem['memory_limit']=self.get_td(html, "Memory limit:")
+		problem['time_limit']=self.get_td(html, "Time limit:")
+		statement=' '.join(html.find(id="problem-body").find_all(text=True,recursive=True))
+		problem['statement']=statement;
+		return problem
+
 class GeneralExtractor:
 	def is_codeforces(self, html):
 		prop = html.find(property="og:site_name")
 		if prop == None:
 			return False
 		return prop['content']=="Codeforces"
+	
+	def is_codechef(self, html):
+		return len(html.find_all(href="https://www.codechef.com/ide"))>0
 
-	def extract(self, path):
+	def is_uri(self, html):
+		return False
+		
+	def is_spoj(self, html):
+		prop = html.find(property="og:site_name")
+		if prop == None:
+			return False
+		return prop['content']=="spoj.com"
+
+	def extract(self, path, wpath="result.json"):
 		fl = open(path,"r")
 		html = BeautifulSoup(fl, 'html.parser')
 		extractor = {}
 		if self.is_codeforces(html):
 			extractor = CodeforcesExtractor()
-		else:
+		elif self.is_codechef(html):
 			extractor = CodechefExtractor()
+		elif self.is_uri(html):
+			print("Not done yet")
+		elif self.is_spoj(html):
+			extractor = SpojExtractor()
+		else:
+			print("Error")
 		problem = extractor.process(html)
 		print(problem)
+		fp = open(wpath, 'w')
+		json.dump(problem, fp)
 
 extr=GeneralExtractor()
 
 extr.extract("a.html")
 extr.extract("b.html")
 extr.extract("chef.html")
+extr.extract("spoj.html")
+
